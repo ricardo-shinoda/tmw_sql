081dc94c89bb9f1ed8d19a780912bec1e480910e
Ricardo Shinoda <ricardoshinoda@gmail.com>
2026-03-11 08:28:11 -0300

    fixing columns names

---
+++ curso-sql/scripts/01_create_table.sql
@@ -0,0 +1,35 @@
+CREATE TABLE clientes (
+    "idCliente" TEXT PRIMARY KEY,
+    "flEmail" INTEGER,
+    "flTwitch" INTEGER,
+    "flYouTube" INTEGER,
+    "flBlueSky" INTEGER,
+    "flInstagram" INTEGER,
+    "qtdePontos" INTEGER,
+    "dtCriacao" TIMESTAMP,
+    "dtAtualizacao" TIMESTAMP
+);
+
+CREATE TABLE produtos (
+    "IdProduto" TEXT PRIMARY KEY,
+    "DescNomeProduto" TEXT,
+    "DescDescricaoProduto" TEXT,
+    "DescCategoriaProduto" TEXT
+);
+
+CREATE TABLE transacoes (
+    "IdTransacao" TEXT PRIMARY KEY,
+    "IdCliente" TEXT,
+    "DtCriacao" TIMESTAMP,
+    "QtdePontos" INTEGER,
+    "DescSistemaOrigem" TEXT
+);
+
+CREATE TABLE transacao_produto (
+    "idTransacaoProduto" TEXT PRIMARY KEY,
+    "IdTransacao" TEXT,
+    "IdProduto" TEXT,
+    "QtdeProduto" INTEGER,
+    "vlProduto" INTEGER
+);
+
